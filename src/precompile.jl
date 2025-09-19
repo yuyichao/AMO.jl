c82335362727972e3595ff499cc6c37cb4fb888a
Yichao Yu <yyc1992@gmail.com>
2025-09-19 16:09:46 -0400

    Precompilation

--- src/precompile.jl
+++ src/precompile.jl
@@ -4,3 +4,10 @@ precompile(Math.genlaguerre, (Int, Float32, Float32))
 precompile(Math.genlaguerre, (Int, Float64, Float64))
 precompile(Math.genlaguerre, (Int, Float64, Int))
 precompile(Math.genlaguerre, (Int, Float32, Int))
+for T in (Bool, Int8, UInt8, Int16, UInt16, Int32, UInt32,
+          Int64, UInt64, Int128, UInt128)
+    precompile(rand, (typeof(RandSetBits{T}(0.0)),))
+    precompile(rand, (typeof(RandDepol{T}(0.0)),))
+    precompile(rand, (typeof(Rand2QDepol{T}(0.0)),))
+    precompile(rand_setbits, (T, Float64))
+end
