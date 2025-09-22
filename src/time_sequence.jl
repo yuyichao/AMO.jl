6d65e6157f4d8aa284149efc2323289c2bc95a69
Yichao Yu <yyc1992@gmail.com>
2025-09-22 16:07:26 -0400

    Support non bitstype values

--- src/time_sequence.jl
+++ src/time_sequence.jl
@@ -29,17 +29,13 @@ get_init(T) = nothing
 get_mul(T) = *
 get_mul!(T) = nothing
 
-_fill_cb(ary, cb) = for i in eachindex(ary)
-    ary[i] = cb()
-end
-
-struct Sequence{OP,NSteps,Steps<:NTuple{NSteps,AbstractStep},NParams,NSteps1,Mul,Mul!} <: AbstractStep{OP,NParams}
+struct Sequence{OP,NSteps,Steps<:NTuple{NSteps,AbstractStep},NParams,ValBuf,GradBuf,PartialBuf,TmpBuf,Mul,Mul!} <: AbstractStep{OP,NParams}
     steps::Steps
-    val_buff::MVector{NSteps,OP}
-    grad_buff::MVector{NParams,OP}
-    prefix_buff::MVector{NSteps1,OP}
-    suffix_buff::MVector{NSteps1,OP}
-    tmp_buff::MVector{2,OP} # [Result, intermediate for gradient]
+    val_buf::ValBuf
+    grad_buf::GradBuf
+    prefix_buf::PartialBuf
+    suffix_buf::PartialBuf
+    tmp_buf::TmpBuf # [Result, intermediate for gradient]
     mul::Mul
     mul!::Mul!
 
@@ -49,19 +45,32 @@ struct Sequence{OP,NSteps,Steps<:NTuple{NSteps,AbstractStep},NParams,NSteps1,Mul
         if mul! !== nothing
             @assert init !== nothing
             @assert mul === nothing
+        else
+            @assert init === nothing
         end
         NParams = sum(nparams, steps)
-        s = new{OP,NSteps,Steps,NParams,NSteps - 1,Mul,Mul!}(
-            steps, MVector{NSteps,OP}(undef), MVector{NParams,OP}(undef),
-            MVector{NSteps - 1,OP}(undef), MVector{NSteps - 1,OP}(undef),
-            MVector{2,OP}(undef), mul, mul!)
+        tmp_buf = nothing
         if init !== nothing
-            _fill_cb(s.val_buff, init)
-            _fill_cb(s.grad_buff, init)
-            _fill_cb(s.prefix_buff, init)
-            _fill_cb(s.suffix_buff, init)
-            _fill_cb(s.tmp_buff, init)
+            val_buf = MVector(ntuple(_->init()::OP, NSteps))
+            grad_buf = MVector(ntuple(_->init()::OP, NParams))
+            prefix_buf = MVector(ntuple(_->init()::OP, NSteps - 1))
+            suffix_buf = MVector(ntuple(_->init()::OP, NSteps - 1))
+            tmp_buf = MVector(ntuple(_->init()::OP, 2))
+        elseif isbitstype(OP)
+            val_buf = MVector{NSteps,OP}(undef)
+            grad_buf = MVector{NParams,OP}(undef)
+            prefix_buf = MVector{NSteps - 1,OP}(undef)
+            suffix_buf = MVector{NSteps - 1,OP}(undef)
+        else
+            val_buf = Vector{OP}(undef, NSteps)
+            grad_buf = Vector{OP}(undef, NParams)
+            prefix_buf = Vector{OP}(undef, NSteps - 1)
+            suffix_buf = Vector{OP}(undef, NSteps - 1)
         end
+        s = new{OP,NSteps,Steps,NParams,typeof(val_buf),typeof(grad_buf),
+                typeof(prefix_buf),typeof(tmp_buf),Mul,Mul!}(
+                    steps, val_buf, grad_buf, prefix_buf, suffix_buf,
+                    tmp_buf, mul, mul!)
         return s
     end
 end
@@ -91,8 +100,8 @@ end
 @generated function _eval_compute(s::Sequence{OP}, grads) where OP
     ex = quote
         steps = s.steps
-        grad_buff = s.grad_buff
-        val_buff = s.val_buff
+        grad_buf = s.grad_buf
+        val_buf = s.val_buf
     end
     starts, ends = _param_range(s)
     ex1 = quote end
@@ -100,12 +109,12 @@ end
     for (i, (start_idx, end_idx)) in enumerate(zip(starts, ends))
         push!(ex1.args, :(
             @inbounds begin
-                @inline val_buff[$i] = compute(steps[$i], SVector{0,OP}())
+                @inline val_buf[$i] = compute(steps[$i], SVector{0,OP}())
             end))
         push!(ex2.args, :(
             @inbounds begin
-                @inline val_buff[$i] =
-                    compute(steps[$i], @view grad_buff[$start_idx:$end_idx])
+                @inline val_buf[$i] =
+                    compute(steps[$i], @view grad_buf[$start_idx:$end_idx])
             end))
     end
     push!(ex.args, quote
@@ -161,40 +170,40 @@ function compute(s::Sequence{OP,NSteps,Steps,NParams}, grads) where {OP,NSteps,S
     mul! = s.mul!
     starts, ends = _param_range(typeof(s))
     @inline _eval_compute(s, grads)
-    @inbounds s.prefix_buff[1] = prev = s.val_buff[1]
+    @inbounds s.prefix_buf[1] = prev = s.val_buf[1]
     @inbounds for i in 2:NSteps - 1
-        prev = @pick_mulass(mul!, mul, s.prefix_buff[i],
-                            prev, s.val_buff[i])
+        prev = @pick_mulass(mul!, mul, s.prefix_buf[i],
+                            prev, s.val_buf[i])
     end
-    last_val = @inbounds s.val_buff[NSteps]
-    res = @inbounds @pick_mul(mul!, mul, s.tmp_buff[1], prev, last_val)
+    last_val = @inbounds s.val_buf[NSteps]
+    res = @inbounds @pick_mul(mul!, mul, s.tmp_buf[1], prev, last_val)
     if isempty(grads)
         return res
     end
     @assert length(grads) == NParams
-    @inbounds s.suffix_buff[NSteps - 1] = prev = last_val
+    @inbounds s.suffix_buf[NSteps - 1] = prev = last_val
     @inbounds for i in NSteps - 2:-1:1
-        prev = @pick_mulass(mul!, mul, s.suffix_buff[i],
-                            s.val_buff[i + 1], prev)
+        prev = @pick_mulass(mul!, mul, s.suffix_buf[i],
+                            s.val_buf[i + 1], prev)
     end
     @inbounds for step_idx in 1:NSteps
         pstart = starts[step_idx]
         pend = ends[step_idx]
         if step_idx == 1
-            suffix = s.suffix_buff[step_idx]
+            suffix = s.suffix_buf[step_idx]
             for param_idx in pstart:pend
-                @pick_mulass(mul!, mul, grads[param_idx], s.grad_buff[param_idx], suffix)
+                @pick_mulass(mul!, mul, grads[param_idx], s.grad_buf[param_idx], suffix)
             end
         elseif step_idx == NSteps
-            prefix = s.prefix_buff[step_idx - 1]
+            prefix = s.prefix_buf[step_idx - 1]
             for param_idx in pstart:pend
-                @pick_mulass(mul!, mul, grads[param_idx], prefix, s.grad_buff[param_idx])
+                @pick_mulass(mul!, mul, grads[param_idx], prefix, s.grad_buf[param_idx])
             end
         else
-            prefix = s.prefix_buff[step_idx - 1]
-            suffix = s.suffix_buff[step_idx]
+            prefix = s.prefix_buf[step_idx - 1]
+            suffix = s.suffix_buf[step_idx]
             for param_idx in pstart:pend
-                tmp = @pick_mul(mul!, mul, s.tmp_buff[2], prefix, s.grad_buff[param_idx])
+                tmp = @pick_mul(mul!, mul, s.tmp_buf[2], prefix, s.grad_buf[param_idx])
                 @pick_mulass(mul!, mul, grads[param_idx], tmp, suffix)
             end
         end
--- test/time_sequence.jl
+++ test/time_sequence.jl
@@ -5,6 +5,7 @@ module TestTimeSequence
 import AMO.TimeSequence as TS
 
 using Test
+using Random
 
 mutable struct Step1{VT} <: TS.AbstractStep{VT,1}
     v1::Float64
@@ -95,6 +96,97 @@ end
     s3 = TS.Sequence{Float64}((Step3{Float64}(),))
     @test TS.nparams(s3) == 3
     test_scalar_sequence(s3)
+end
+
+mutable struct SStep1 <: TS.AbstractStep{String,1}
+    v1::String
+    SStep1() = new()
+end
+function TS.set_params!(op::SStep1, params)
+    op.v1 = params[1]
+    return
+end
+function TS.compute(op::SStep1, grad)
+    if !isempty(grad)
+        grad[1] = "d"
+    end
+    return op.v1
+end
+mutable struct SStep2 <: TS.AbstractStep{String,2}
+    v1::String
+    v2::String
+    SStep2() = new()
+end
+function TS.set_params!(op::SStep2, params)
+    op.v1 = params[1]
+    op.v2 = params[2]
+    return
+end
+function TS.compute(op::SStep2, grad)
+    if !isempty(grad)
+        grad[1] = "d" * op.v2
+        grad[2] = op.v1 * "d"
+    end
+    return op.v1 * op.v2
+end
+mutable struct SStep3 <: TS.AbstractStep{String,3}
+    v1::String
+    v2::String
+    v3::String
+    SStep3() = new()
+end
+function TS.set_params!(op::SStep3, params)
+    op.v1 = params[1]
+    op.v2 = params[2]
+    op.v3 = params[3]
+    return
+end
+function TS.compute(op::SStep3, grad)
+    if !isempty(grad)
+        grad[1] = "d" * op.v2 * op.v3
+        grad[2] = op.v1 * "d" * op.v3
+        grad[3] = op.v1 * op.v2 * "d"
+    end
+    return op.v1 * op.v2 * op.v3
+end
+
+function test_string_sequence(s)
+    NP = TS.nparams(typeof(s))
+    @test TS.nparams(s) == NP
+
+    grads = fill("", NP)
+    for _ in 1:100
+        params = [randstring(2) for _ in 1:NP]
+        TS.set_params!(s, params)
+        @test TS.compute(s, String[]) == prod(params)
+        grads .= ""
+        @test TS.compute(s, grads) == prod(params)
+        for i in 1:NP
+            @test grads[i] == prod(((pidx == i ? "d" : p)
+                                    for (pidx, p) in enumerate(params)))
+        end
+    end
+end
+
+@testset "String Time Sequence" begin
+    ops = (SStep1(), SStep2(), SStep3(), SStep1(),
+           SStep1(), SStep2(), SStep3(), SStep2(),
+           SStep2(), SStep1(), SStep3(), SStep3())
+    s = TS.Sequence{String}(ops)
+    @test TS.nparams(s) == 24
+    test_string_sequence(s)
+
+    s1 = TS.Sequence{String}((SStep1(),))
+    @test TS.nparams(s1) == 1
+    test_string_sequence(s1)
+
+    s2 = TS.Sequence{String}((SStep2(),))
+    @test TS.nparams(s2) == 2
+    test_string_sequence(s2)
+
+    s3 = TS.Sequence{String}((SStep3(),))
+    @test TS.nparams(s3) == 3
+    test_string_sequence(s3)
 end
 
 end
