7341528bf668fdfc43a40ad35fd82d9b1844c86c
Yichao Yu <yyc1992@gmail.com>
2025-10-06 14:58:30 -0400

    Add reduce matrix element calculation

--- src/atomic.jl
+++ src/atomic.jl
@@ -2,6 +2,9 @@
 
 module Atomic
 
+using RationalRoots
+using WignerSymbols
+
 public g_sum, g_s, g_l
 
 function g_sum(J, J1, g1, J2, g2)
@@ -11,5 +14,16 @@ end
 
 const g_s = 2.00231930436092
 const g_l = 1.0
+
+_sqrt(::Type{RationalRoot{T}}, v) where T = signedroot(RationalRoot{T}, v)
+_sqrt(::Type{T}, v) where T = sqrt(T(v))
+
+function couple_reduced_element(::Type{T}, J1′, J2′, J0, J1, J2, k=1) where T<:Real
+    r = _sqrt(T, (2 * J1 + 1) * (2 * J2′ + 1)) * wigner6j(T, J1, J2, k, J2′, J1′, J0)
+    return isodd(Int(J0 + J1 + J2′ + k)) ? -r : r
+end
+
+couple_reduced_element(J1′, J2′, J0, J1, J2, k=1) =
+    couple_reduced_element(RationalRoot{BigInt}, J1′, J2′, J0, J1, J2, k)
 
 end
--- test/atomic.jl
+++ test/atomic.jl
@@ -2,9 +2,12 @@
 
 module TestAtomic
 
-using AMO: g_sum, g_s, g_l
+using AMO: g_sum, g_s, g_l, Atomic
 
 using Test
+using HalfIntegers
+using RationalRoots
+using WignerSymbols
 
 @testset "g-factor" begin
     @test g_s ≈ 2 + 1 / 137 / π atol=0.5e-4
@@ -17,6 +20,77 @@ using Test
 
     @test g_sum(3/2, 1, 1, 1/2, 2) ≈ 4/3
     @test g_sum(3/2, 1/2, 2, 1, 1) ≈ 4/3
+end
+
+function coupling_kernel(j0, m0, j1, m1, j2, m2, j1′, m1′, j2′, m2′, k, q)
+    return clebschgordan(Float64, j1, m1, j0, m0, j1′, m1′) *
+        clebschgordan(Float64, j2, m2, j0, m0, j2′, m2′) *
+        clebschgordan(Float64, j2, m2, k, q, j1, m1)
+end
+
+function coupling0(j0, j1, j2, j1′, m1′, j2′, m2′, k, q)
+    return sum(coupling_kernel(j0, m0, j1, m1, j2, m2, j1′, m1′, j2′, m2′, k, q)
+               for m0 in -j0:j0, m1 in -j1:j1, m2 in -j2:j2; init=0.0)
+end
+
+function check_coupling_js(j0, j1, j2, k)
+    has_nonzero = false
+    vtotal = 0.0
+    for j1′ in abs(j1 - j0):(j1 + j0)
+        for j2′ in abs(j2 - j0):(j2 + j0)
+            vbig = Atomic.couple_reduced_element(j1′, j2′, j0, j1, j2, k)
+            @test vbig isa RationalRoot{BigInt}
+            vf = Atomic.couple_reduced_element(Float64, j1′, j2′, j0, j1, j2, k)
+            @test vf isa Float64
+            if abs(j1′ - j2′) > k || j1′ + j2′ < k
+                @test vbig == 0
+                @test vf == 0
+                continue
+            end
+            @test vf ≈ vbig atol=sqrt(eps())
+            vtotal_j = 0.0
+            for m1′ in -j1′:j1′
+                for m2′ in -j2′:j2′
+                    q = m1′ - m2′
+                    if q < -k || q > k
+                        continue
+                    end
+                    v0 = coupling0(j0, j1, j2, j1′, m1′, j2′, m2′, k, q)
+                    v1 = vf * clebschgordan(Float64, j2′, m2′, k, q, j1′, m1′)
+                    @test v0 ≈ v1 atol=sqrt(eps())
+                    has_nonzero |= !(isapprox(v0, 0, atol=sqrt(eps())))
+                    vtotal_j += abs2(v1)
+                end
+            end
+            @test vtotal_j ≈ abs2(vf) * (2 * j1′ + 1)
+            vtotal += vtotal_j
+        end
+    end
+    @test has_nonzero
+    @test vtotal ≈ (2 * j1 + 1) * (2 * j0 + 1)
+end
+
+function check_coupling(jmax, kmax)
+    for dk in 0:twice(kmax)
+        k = half(dk)
+        for dj0 in 0:twice(jmax)
+            j0 = half(dj0)
+            for dj1 in 0:twice(jmax)
+                j1 = half(dj1)
+                for dj2 in 0:twice(jmax)
+                    j2 = half(dj2)
+                    if abs(j1 - j2) > k || j1 + j2 < k || (dj1 - dj2 - dk) % 2 != 0
+                        continue
+                    end
+                    check_coupling_js(j0, j1, j2, k)
+                end
+            end
+        end
+    end
+end
+
+@testset "Reduced matrix element" begin
+    @time check_coupling(4, 2)
 end
 
 end
