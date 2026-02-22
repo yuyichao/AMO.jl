22e40a6312abb4ddf62be510216cca9e13648abf
Yichao Yu <yyc1992@gmail.com>
2026-02-21 22:34:11 -0500

    Add document

--- src/atomic.jl
+++ src/atomic.jl
@@ -7,17 +7,51 @@ using WignerSymbols
 
 public g_sum, g_s, g_l
 
+"""
+    g_sum(J, J1, g1, J2, g2)
+
+Compute the g-factor for angular momentum `J` that is created from `J1` with g-factor `g1`
+and `J2` with g-factor `g2`.
+"""
 function g_sum(J, J1, g1, J2, g2)
     return (g1 * (J * (J + 1) + J1 * (J1 + 1) - J2 * (J2 + 1)) +
         g2 * (J * (J + 1) + J2 * (J2 + 1) - J1 * (J1 + 1))) / (2 * J * (J + 1))
 end
 
+"""
+    g_s
+
+g-factor for electron spin
+"""
 const g_s = 2.00231930436092
+
+
+"""
+    g_l = 1.0
+
+g-factor for electron orbit
+"""
 const g_l = 1.0
 
 _sqrt(::Type{RationalRoot{T}}, v) where T = signedroot(RationalRoot{T}, v)
 _sqrt(::Type{T}, v) where T = sqrt(T(v))
 
+"""
+    couple_reduced_element(::Type{T}=RationalRoot{BigInt}, J1′, J2′, J0, J1, J2, k=1)
+
+Compute the ratio `R` between the reduced matrix elements for `(J1,J2)` and `(J1′,J2′)`.
+i.e.,
+
+    ⟨J1′||T^k||J2′⟩ = R ⟨J1||T^k||J2⟩
+
+The definition of the reduced matrix element follows the Wigner-Eckart theorem
+with Clebsh-Gordan coefficient:
+
+    ⟨J1,m_J1|T^k_q|J2,m_J2⟩=⟨J1||T^k||J2⟩⟨J1,m_J1|J2,m_J2;k,q⟩
+
+`J1′` and `J2′` are created from another angular momentum `J0` coupling to `J1` and `J2`
+respectively. `J0` is assumed to be not coupled to the tensor operator `T`.
+"""
 function couple_reduced_element(::Type{T}, J1′, J2′, J0, J1, J2, k=1) where T<:Real
     r = _sqrt(T, (2 * J1 + 1) * (2 * J2′ + 1)) * wigner6j(T, J1, J2, k, J2′, J1′, J0)
     return isodd(Int(J0 + J1 + J2′ + k)) ? -r : r
