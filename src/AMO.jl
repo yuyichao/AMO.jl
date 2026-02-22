547f9a68c8b630e09b869c7c8e88b3f3cfdace16
Yichao Yu <yyc1992@gmail.com>
2026-02-21 22:33:57 -0500

    Make Atomic submodule public

--- src/AMO.jl
+++ src/AMO.jl
@@ -10,7 +10,7 @@ public RandSetBits, rand_setbits, RandDepol, Rand2QDepol
 
 include("atomic.jl")
 using .Atomic: g_sum, g_s, g_l
-public g_sum, g_s, g_l
+public Atomic, g_sum, g_s, g_l
 
 include("trap.jl")
 
